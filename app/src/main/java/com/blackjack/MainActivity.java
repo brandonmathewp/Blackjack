f72b5648449c5fbe30e22cb2cb37b45012ffebaa
brandonmathewp <brandon.mathew.p@gmail.com>
2026-04-24 12:40:26 -1000

    Initial Commit: Skeleton Code

---
+++ app/src/main/java/com/blackjack/MainActivity.java
@@ -0,0 +1,17 @@
+package com.blackjack;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
